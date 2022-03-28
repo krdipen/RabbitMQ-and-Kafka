091e087b2045759f9930bfb1fa7a38029a2274e0
Dipen Kumar <kr.dipen4@gmail.com>
2022-03-28 14:24:19 +0530

    latency-test added

--- myprofile/views.py
+++ myprofile/views.py
@@ -198,56 +198,60 @@ def runsimulation(request):
                 else:
                     return JsonResponse({"error": "order type invalid"})
         end = time.time()
-        return JsonResponse({"time": f"{str(end - start)}"})
+        return JsonResponse({"time": f"{str(end - start)}", "start": f"{str(start)}"})
     return JsonResponse({"error": "method not supported"})
 
 def runkafkaserver(request):
     if request.method == "GET":
         start = time.time()
-        for msg in KAFKA_CONSUMER:
-            if msg.key.decode("utf-8") == "bill":
-                args = [arg.strip() for arg in msg.value.decode("utf-8").split(',')]
-                orderid = args[0]
-                type = args[1]
-                ord = Orders.objects.get(id=orderid)
-                inv1 = Inventory.objects.get(id=ord.item1id)
-                inv2 = Inventory.objects.get(id=ord.item2id)
-                inv3 = Inventory.objects.get(id=ord.item3id)
-                totalcost = inv1.unitcost * ord.count1 + inv2.unitcost * ord.count2 + inv3.unitcost * ord.count3
-                modified = ord.modified
-                bll = Bills()
-                bll.orderid = orderid
-                bll.totalcost = totalcost
-                bll.modified = modified
-                bll.type = type
-                bll.save()
-                if type == "retail":
-                    key = bytes("fulfill", encoding='utf-8')
-                    value = bytes(f"{bll.id}", encoding='utf-8')
-                    KAFKA_PRODUCER.send('orders', key=key, value=value)
-                elif type == "refund":
-                    key = bytes("notification", encoding='utf-8')
-                    value = bytes(f"{orderid},refunded", encoding='utf-8')
-                    KAFKA_PRODUCER.send('orders', key=key, value=value)
-                else:
-                    pass # print("bill type invalid")
-            elif msg.key.decode("utf-8") == "fulfill":
-                args = [arg.strip() for arg in msg.value.decode("utf-8").split(',')]
-                billid = args[0]
-                bll = Bills.objects.get(id=billid)
-                ord = Orders.objects.get(id=bll.orderid)
-                if ord.modified == bll.modified and ord.status == "in process":
-                    ord.status = "shipped"
-                    ord.save()
-                    key = bytes("notification", encoding='utf-8')
-                    value = bytes(f"{ord.id},shipped", encoding='utf-8')
-                    KAFKA_PRODUCER.send('orders', key=key, value=value)
-            elif msg.key.decode("utf-8") == "notification":
-                args = [arg.strip() for arg in msg.value.decode("utf-8").split(',')]
-                orderid = args[0]
-                type = args[1]
-                with open("data/notifications.out", "a") as file:
-                    file.write(f"{type} | OrderID: {orderid}\n")
+        stop = request.GET["stop"]
+        while True:
+            for msg in KAFKA_CONSUMER:
+                if msg.key.decode("utf-8") == "bill":
+                    args = [arg.strip() for arg in msg.value.decode("utf-8").split(',')]
+                    orderid = args[0]
+                    type = args[1]
+                    ord = Orders.objects.get(id=orderid)
+                    inv1 = Inventory.objects.get(id=ord.item1id)
+                    inv2 = Inventory.objects.get(id=ord.item2id)
+                    inv3 = Inventory.objects.get(id=ord.item3id)
+                    totalcost = inv1.unitcost * ord.count1 + inv2.unitcost * ord.count2 + inv3.unitcost * ord.count3
+                    modified = ord.modified
+                    bll = Bills()
+                    bll.orderid = orderid
+                    bll.totalcost = totalcost
+                    bll.modified = modified
+                    bll.type = type
+                    bll.save()
+                    if type == "retail":
+                        key = bytes("fulfill", encoding='utf-8')
+                        value = bytes(f"{bll.id}", encoding='utf-8')
+                        KAFKA_PRODUCER.send('orders', key=key, value=value)
+                    elif type == "refund":
+                        key = bytes("notification", encoding='utf-8')
+                        value = bytes(f"{orderid},refunded", encoding='utf-8')
+                        KAFKA_PRODUCER.send('orders', key=key, value=value)
+                    else:
+                        pass # print("bill type invalid")
+                elif msg.key.decode("utf-8") == "fulfill":
+                    args = [arg.strip() for arg in msg.value.decode("utf-8").split(',')]
+                    billid = args[0]
+                    bll = Bills.objects.get(id=billid)
+                    ord = Orders.objects.get(id=bll.orderid)
+                    if ord.modified == bll.modified and ord.status == "in process":
+                        ord.status = "shipped"
+                        ord.save()
+                        key = bytes("notification", encoding='utf-8')
+                        value = bytes(f"{ord.id},shipped", encoding='utf-8')
+                        KAFKA_PRODUCER.send('orders', key=key, value=value)
+                elif msg.key.decode("utf-8") == "notification":
+                    args = [arg.strip() for arg in msg.value.decode("utf-8").split(',')]
+                    orderid = args[0]
+                    type = args[1]
+                    with open("data/notifications.out", "a") as file:
+                        file.write(f"{type} | OrderID: {orderid}\n")
+            if stop == "true":
+                break
         end = time.time()
         return JsonResponse({"time": f"{str(end - start)}"})
     return JsonResponse({"error": "method not supported"})
